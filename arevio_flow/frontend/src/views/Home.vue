fe036d676fd50f2d515dd6d6fbba72857bbe74be
gbiebuyc <gbiebuyc@users.noreply.github.com>
2019-08-27 17:22:28 +0200

    Change home page layout

--- arevio_flow/frontend/src/views/Home.vue
+++ arevio_flow/frontend/src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
-  <div class="home">
+  <div class="home" style="padding: 20px;">
     <h1>Home</h1>
-    <ul v-if="companies.length">
-      <li v-for="c in companies" :key="c.url">
-        <router-link to="">{{ c.name }}</router-link>
-      </li>
-    </ul>
+    <div v-if="companies.length" class="card-columns">
+      <div v-for="c in companies" :key="c.url" class="card">
+        <img src="https://www.royalcontainers.com/wp-content/uploads/2017/03/logo-placeholder.jpeg" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">{{ c.name }}</h5>
+          <router-link class="stretched-link" to=""></router-link>
+        </div>
+      </div>
+    </div>
     <p v-else>no companies</p>
   </div>
 </template>
